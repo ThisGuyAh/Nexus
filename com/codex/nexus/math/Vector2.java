99a8e5a1385f6d2af34d1377c4cefba40dcee068
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-05-30 11:41:23 -0400

    Create Vector2.java

---
+++ com/codex/nexus/math/Vector2.java
@@ -0,0 +1,67 @@
+package com.codex.nexus.math;
+
+public class Vector2 {
+
+	public float x;
+	public float y;
+
+	public Vector2() {
+		setZero();
+	}
+
+	public Vector2(Vector2 other) {
+		x = other.x;
+		y = other.y;
+	}
+
+	public Vector2(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public float getMagnitude() {
+		return (float) Math.sqrt(x * x + y * y);
+	}
+
+	public float getDot(Vector2 other) {
+		return x * other.x + y * other.y;
+	}
+
+	public Vector2 setZero() {
+		x = 0.0F;
+		y = 0.0F;
+
+		return this;
+	}
+
+	public Vector2 add(Vector3 other) {
+		x += other.x;
+		y += other.y;
+
+		return this;
+	}
+
+	public Vector2 subtract(Vector3 other) {
+		x -= other.x;
+		y -= other.y;
+
+		return this;
+	}
+
+	public Vector2 negate() {
+		x = -x;
+		y = -y;
+
+		return this;
+	}
+
+	public Vector2 normalize() {
+		float magnitude = getMagnitude();
+
+		x /= magnitude;
+		y /= magnitude;
+
+		return this;
+	}
+
+}
