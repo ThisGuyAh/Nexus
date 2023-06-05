f0718e180128490f61b1b00d17acf2b49540169d
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-06-05 15:30:31 -0400

    Update VertexArray.java

--- com/codex/nexus/memory/VertexArray.java
+++ com/codex/nexus/memory/VertexArray.java
@@ -1,15 +1,57 @@
 package com.codex.nexus.memory;
 
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL30.*;
 
 public class VertexArray {
 
+	private VertexBuffer[] vertexBuffers;
 	private int handle;
+	private IndexBuffer indexBuffer;
 
-	public VertexArray() {
+	public VertexArray(VertexBuffer... vertexBuffers) {
+		this.vertexBuffers = vertexBuffers;
 		handle = glGenVertexArrays();
 
 		bind();
+
+		for (VertexBuffer vertexBuffer : vertexBuffers) {
+			vertexBuffer.bind();
+
+			InputLayout inputLayout = vertexBuffer.getInputLayout();
+			InputElement[] inputElements = inputLayout.getInputElements();
+			int offset = 0;
+
+			for (int i = 0; i < inputElements.length; i++) {
+				int componentCount = inputElements[i].getComponentCount();
+
+				glVertexAttribPointer(i, componentCount, GL_FLOAT, false, inputLayout.getStride(), offset);
+				offset += componentCount * Float.BYTES;
+			}
+		}
+
+		unbind();
+	}
+
+	public VertexBuffer[] getVertexBuffers() {
+		return vertexBuffers;
+	}
+	
+	public int getHandle() {
+		return handle;
+	}
+
+	public IndexBuffer getIndexBuffer() {
+		return indexBuffer;
+	}
+
+	public void setIndexBuffer(IndexBuffer indexBuffer) {
+		this.indexBuffer = indexBuffer;
+
+		bind();
+		indexBuffer.bind();
+		unbind();
 	}
 
 	public void bind() {
