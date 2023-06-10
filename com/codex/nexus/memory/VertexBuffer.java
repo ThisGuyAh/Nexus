aecd6c725b4f5232e59144020e69306042083580
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-06-10 05:23:08 -0400

    Update VertexBuffer.java

--- com/codex/nexus/memory/VertexBuffer.java
+++ com/codex/nexus/memory/VertexBuffer.java
@@ -3,42 +3,95 @@ package com.codex.nexus.memory;
 import static org.lwjgl.BufferUtils.*;
 import static org.lwjgl.opengl.GL15.*;
 
+/**
+ * {@code VertexBuffer} stores vertex data and an {@code InputLayout}.
+ * 
+ * @see InputLayout
+ * @see VertexArray
+ * 
+ * @author Christopher Ruley
+ */
 public class VertexBuffer {
 
-	private InputLayout inputLayout;
+	/**
+	 * The stored data.
+	 */
 	private float[] data;
+
+	/**
+	 * The {@code InputLayout}.
+	 */
+	private InputLayout inputLayout;
+
+	/**
+	 * The unique identifier.
+	 */
 	private int handle;
 
-	public VertexBuffer(InputLayout inputLayout, float[] data) {
-		this.inputLayout = inputLayout;
+	/**
+	 * Constructs a {@code VertexBuffer}.
+	 * 
+	 * @param data the data to store.
+	 * @param inputLayout the {@code InputLayout}.
+	 */
+	public VertexBuffer(float[] data, InputLayout inputLayout) {
 		this.data = data;
-
+		this.inputLayout = inputLayout;
 		handle = glGenBuffers();
 
 		bind();
-		glBufferData(GL_ARRAY_BUFFER, createFloatBuffer(data.length).put(data).flip(), GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER,
+				createFloatBuffer(data.length).put(data).flip(),
+				GL_STATIC_DRAW);
 	}
 
-	public InputLayout getInputLayout() {
-		return inputLayout;
-	}
-
+	/**
+	 * Gets the stored data.
+	 * 
+	 * @return the stored data.
+	 */
 	public float[] getData() {
 		return data;
 	}
 
-	public int getHandle() {
-		return handle;
+	/**
+	 * Gets the {@code InputLayout}.
+	 * 
+	 * @return the {@code InputLayout}.
+	 */
+	public InputLayout getInputLayout() {
+		return inputLayout;
 	}
 
+	/**
+	 * Sets the stored data.
+	 * 
+	 * @param data the data to store.
+	 */
+	public void setData(float[] data) {
+		this.data = data;
+
+		bind();
+		glBufferSubData(GL_ARRAY_BUFFER, 0, data);
+	}
+
+	/**
+	 * Binds this {@code VertexBuffer}.
+	 */
 	public void bind() {
 		glBindBuffer(GL_ARRAY_BUFFER, handle);
 	}
 
+	/**
+	 * Unbinds this {@code VertexBuffer}.
+	 */
 	public void unbind() {
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
 	}
 
+	/**
+	 * Deletes this {@code VertexBuffer}.
+	 */
 	public void delete() {
 		glDeleteBuffers(handle);
 	}
