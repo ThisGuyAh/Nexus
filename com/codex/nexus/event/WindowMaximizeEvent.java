f3edb18d85df41e1163abf953aba20ef0763b191
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-06-25 13:30:27 -0400

    Update WindowMaximizeEvent.java

--- com/codex/nexus/event/WindowMaximizeEvent.java
+++ com/codex/nexus/event/WindowMaximizeEvent.java
@@ -5,13 +5,20 @@ import com.codex.nexus.core.Window;
 public class WindowMaximizeEvent {
 	
 	private Window window;
+
+	private boolean maximized;
 	
-	public WindowMaximizeEvent(Window window) {
+	public WindowMaximizeEvent(Window window, boolean maximized) {
 		this.window = window;
+		this.maximized = maximized;
 	}
 	
 	public Window getWindow() {
 		return window;
+	}
+
+	public boolean isMaximized() {
+		return maximized;
 	}
 
 }
