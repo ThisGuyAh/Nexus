a67dcf3d662fa19a0cf92e80bab2d3c2e68e9dab
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-02 14:00:29 -0400

    Update Methods.java

--- com/codex/nexus/utility/Methods.java
+++ com/codex/nexus/utility/Methods.java
@@ -9,28 +9,28 @@ import java.util.List;
 
 public class Methods {
 
-	private Methods() {
-	}
+    private Methods() {
+    }
 
-	public static List<Method> findWithAnnotation(Class<? extends Annotation> annotationClass, Class<?> clazz) {
-		Method[] methods = clazz.getMethods();
+    public static List<Method> findWithAnnotation(Class<? extends Annotation> annotationClass, Class<?> clazz) {
+        Method[] methods = clazz.getMethods();
 
-		return stream(methods).filter(method -> method.isAnnotationPresent(annotationClass)).collect(toList());
-	}
+        return stream(methods).filter(method -> method.isAnnotationPresent(annotationClass)).collect(toList());
+    }
 
-	public static List<Method> findWithAnnotation(Class<? extends Annotation> annotationClass,
-			List<Method> methods) {
-		return methods.stream().filter(method -> method.isAnnotationPresent(annotationClass)).collect(toList());
-	}
+    public static List<Method> findWithAnnotation(Class<? extends Annotation> annotationClass,
+                                                  List<Method> methods) {
+        return methods.stream().filter(method -> method.isAnnotationPresent(annotationClass)).collect(toList());
+    }
 
-	public static List<Method> findWithParameterCount(int parameterCount, Class<?> clazz) {
-		Method[] methods = clazz.getMethods();
+    public static List<Method> findWithParameterCount(int parameterCount, Class<?> clazz) {
+        Method[] methods = clazz.getMethods();
 
-		return stream(methods).filter(method -> method.getParameterCount() == parameterCount).collect(toList());
-	}
+        return stream(methods).filter(method -> method.getParameterCount() == parameterCount).collect(toList());
+    }
 
-	public static List<Method> findWithParameterCount(int parameterCount, List<Method> methods) {
-		return methods.stream().filter(method -> method.getParameterCount() == parameterCount).collect(toList());
-	}
+    public static List<Method> findWithParameterCount(int parameterCount, List<Method> methods) {
+        return methods.stream().filter(method -> method.getParameterCount() == parameterCount).collect(toList());
+    }
 
 }
