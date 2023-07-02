f15c19c777e7f4d4276153d36da12effca9c6328
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-02 14:52:01 -0400

    Update PipelineStage.java

--- com/codex/nexus/render/PipelineStage.java
+++ com/codex/nexus/render/PipelineStage.java
@@ -9,27 +9,27 @@ import static org.lwjgl.opengl.GL43.*;
  */
 public enum PipelineStage {
 
-	VERTEX,
-	GEOMETRY,
-	TESS_CONTROL,
-	TESS_EVALUATION,
-	FRAGMENT,
-	COMPUTE;
+    VERTEX,
+    GEOMETRY,
+    TESS_CONTROL,
+    TESS_EVALUATION,
+    FRAGMENT,
+    COMPUTE;
 
-	/**
-	 * Gets the OpenGL type corresponding with this {@code PipelineStage}.
-	 *
-	 * @return the OpenGL type.
-	 */
-	public int getOpenGLType() {
-		return switch (this) {
-			case VERTEX -> GL_VERTEX_SHADER;
-			case GEOMETRY -> GL_GEOMETRY_SHADER;
-			case TESS_CONTROL -> GL_TESS_CONTROL_SHADER;
-			case TESS_EVALUATION -> GL_TESS_EVALUATION_SHADER;
-			case FRAGMENT -> GL_FRAGMENT_SHADER;
-			case COMPUTE -> GL_COMPUTE_SHADER;
-		};
-	}
+    /**
+     * Gets the OpenGL type corresponding with this {@code PipelineStage}.
+     *
+     * @return the OpenGL type.
+     */
+    public int getOpenGLType() {
+        return switch (this) {
+            case VERTEX -> GL_VERTEX_SHADER;
+            case GEOMETRY -> GL_GEOMETRY_SHADER;
+            case TESS_CONTROL -> GL_TESS_CONTROL_SHADER;
+            case TESS_EVALUATION -> GL_TESS_EVALUATION_SHADER;
+            case FRAGMENT -> GL_FRAGMENT_SHADER;
+            case COMPUTE -> GL_COMPUTE_SHADER;
+        };
+    }
 
 }
