1d39fa0f42ad5e7f59843c15a52a7ec31e589511
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-02 14:49:04 -0400

    Update Shader.java

--- com/codex/nexus/render/Shader.java
+++ com/codex/nexus/render/Shader.java
@@ -5,77 +5,77 @@ import static org.lwjgl.opengl.GL20.*;
 
 /**
  * A {@code Shader} is a user-defined program designed to run on some stage of a graphics processor.
- * 
+ *
  * @author Christopher Ruley
  */
 public class Shader {
-	
-	/**
-	 * The {@code PipelineStage}.
-	 */
-	private PipelineStage pipelineStage;
-	
-	/**
-	 * The source code.
-	 */
-	private String sourceCode;
 
-	/**
-	 * The unique identification.
-	 */
-	private int handle;
+    /**
+     * The {@code PipelineStage}.
+     */
+    private PipelineStage pipelineStage;
 
-	/**
-	 * Constructs a {@code Shader}.
-	 * 
-	 * @param pipelineStage the {@code PipelineStage}.
-	 * @param sourceCode the source code.
-	 */
-	public Shader(PipelineStage pipelineStage, String sourceCode) {
-		this.pipelineStage = pipelineStage;
-		this.sourceCode = sourceCode;
-		handle = glCreateShader(pipelineStage.getOpenGLType());
+    /**
+     * The source code.
+     */
+    private String sourceCode;
 
-		glShaderSource(handle, sourceCode);
-		glCompileShader(handle);
+    /**
+     * The unique identification.
+     */
+    private int handle;
 
-		if (glGetShaderi(handle, GL_COMPILE_STATUS) == GL_FALSE) {
-			String errorMessage = glGetShaderInfoLog(handle);
+    /**
+     * Constructs a {@code Shader}.
+     *
+     * @param pipelineStage the {@code PipelineStage}.
+     * @param sourceCode the source code.
+     */
+    public Shader(PipelineStage pipelineStage, String sourceCode) {
+        this.pipelineStage = pipelineStage;
+        this.sourceCode = sourceCode;
+        handle = glCreateShader(pipelineStage.getOpenGLType());
 
-			glDeleteShader(handle);
+        glShaderSource(handle, sourceCode);
+        glCompileShader(handle);
 
-			throw new RuntimeException(errorMessage);
-		}
-	}
-	
-	/**
-	 * Gets the {@code PipelineStage}.
-	 */
-	public PipelineStage getPipelineStage() {
-		return pipelineStage;
-	}
-	
-	/**
-	 * Gets the source code.
-	 */
-	public String getSourceCode() {
-		return sourceCode;
-	}
+        if (glGetShaderi(handle, GL_COMPILE_STATUS) == GL_FALSE) {
+            String errorMessage = glGetShaderInfoLog(handle);
 
-	/**
-	 * Gets the unique identification.
-	 * 
-	 * @return the unique identification.
-	 */
-	public int getHandle() {
-		return handle;
-	}
+            glDeleteShader(handle);
 
-	/**
-	 * Deletes this {@code Shader}.
-	 */
-	public void delete() {
-		glDeleteShader(handle);
-	}
+            throw new RuntimeException(errorMessage);
+        }
+    }
+
+    /**
+     * Gets the {@code PipelineStage}.
+     */
+    public PipelineStage getPipelineStage() {
+        return pipelineStage;
+    }
+
+    /**
+     * Gets the source code.
+     */
+    public String getSourceCode() {
+        return sourceCode;
+    }
+
+    /**
+     * Gets the unique identification.
+     *
+     * @return the unique identification.
+     */
+    public int getHandle() {
+        return handle;
+    }
+
+    /**
+     * Deletes this {@code Shader}.
+     */
+    public void delete() {
+        glDeleteShader(handle);
+    }
 
 }
