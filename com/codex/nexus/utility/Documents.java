10c7428a276f9271cdbf11679362e249e155f64f
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-02 14:02:40 -0400

    Update Documents.java

--- com/codex/nexus/utility/Documents.java
+++ com/codex/nexus/utility/Documents.java
@@ -8,77 +8,77 @@ import java.util.List;
 
 public class Documents {
 
-	private Documents() {
-	}
+    private Documents() {
+    }
 
-	public static List<String> read(String path) {
-		List<String> output = new ArrayList<>();
+    public static List<String> read(String path) {
+        List<String> output = new ArrayList<>();
 
-		try {
-			output = Files.readAllLines(Paths.get(path));
-		} catch (IOException exception) {
-			exception.printStackTrace();
-		}
+        try {
+            output = Files.readAllLines(Paths.get(path));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
 
-		return output;
-	}
+        return output;
+    }
 
-	public static List<List<String>> split(String token, List<String> lines) {
-		List<String> current = new ArrayList<>();
-		List<List<String>> output = new ArrayList<>();
-		boolean found = false;
+    public static List<List<String>> split(String token, List<String> lines) {
+        List<String> current = new ArrayList<>();
+        List<List<String>> output = new ArrayList<>();
+        boolean found = false;
 
-		for (String line : lines) {
-			if (line.startsWith(token)) {
-				if (found) {
-					output.add(new ArrayList<>(current));
-					current.clear();
-				} else {
-					found = true;
-				}
-			}
+        for (String line : lines) {
+            if (line.startsWith(token)) {
+                if (found) {
+                    output.add(new ArrayList<>(current));
+                    current.clear();
+                } else {
+                    found = true;
+                }
+            }
 
-			if (found) {
-				current.add(line);
-			}
-		}
+            if (found) {
+                current.add(line);
+            }
+        }
 
-		output.add(new ArrayList<>(current));
+        output.add(new ArrayList<>(current));
 
-		return output;
-	}
+        return output;
+    }
 
-	public static List<List<String>> split(String startToken, String endToken, List<String> lines) {
-		List<String> current = new ArrayList<>();
-		List<List<String>> output = new ArrayList<>();
-		boolean found = false;
+    public static List<List<String>> split(String startToken, String endToken, List<String> lines) {
+        List<String> current = new ArrayList<>();
+        List<List<String>> output = new ArrayList<>();
+        boolean found = false;
 
-		for (String line : lines) {
-			if (line.startsWith(startToken) || line.equalsIgnoreCase(startToken)) {
-				found = true;
-			}
-			if (found) {
-				current.add(line);
-			}
-			if (line.startsWith(endToken) || line.equalsIgnoreCase(endToken)) {
-				output.add(new ArrayList<>(current));
-				current.clear();
+        for (String line : lines) {
+            if (line.startsWith(startToken) || line.equalsIgnoreCase(startToken)) {
+                found = true;
+            }
+            if (found) {
+                current.add(line);
+            }
+            if (line.startsWith(endToken) || line.equalsIgnoreCase(endToken)) {
+                output.add(new ArrayList<>(current));
+                current.clear();
 
-				found = false;
-			}
-		}
+                found = false;
+            }
+        }
 
-		return output;
-	}
+        return output;
+    }
 
-	public static String concatenate(List<String> lines) {
-		StringBuilder stringBuilder = new StringBuilder();
+    public static String concatenate(List<String> lines) {
+        StringBuilder stringBuilder = new StringBuilder();
 
-		for (var line : lines) {
-			stringBuilder.append(line + "\n");
-		}
+        for (var line : lines) {
+            stringBuilder.append(line + "\n");
+        }
 
-		return stringBuilder.toString();
-	}
+        return stringBuilder.toString();
+    }
 
 }
