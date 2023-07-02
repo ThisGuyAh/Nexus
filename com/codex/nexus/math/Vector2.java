2008dee3d566c3a0c7b52cba1e5b3a90c6ac2a7e
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-02 13:52:48 -0400

    Update Vector2.java

--- com/codex/nexus/math/Vector2.java
+++ com/codex/nexus/math/Vector2.java
@@ -2,66 +2,70 @@ package com.codex.nexus.math;
 
 public class Vector2 {
 
-	public float x;
-	public float y;
+    public float x;
+    public float y;
 
-	public Vector2() {
-		setZero();
-	}
+    public Vector2() {
+        setZero();
+    }
 
-	public Vector2(Vector2 other) {
-		x = other.x;
-		y = other.y;
-	}
+    public Vector2(Vector2 other) {
+        x = other.x;
+        y = other.y;
+    }
 
-	public Vector2(float x, float y) {
-		this.x = x;
-		this.y = y;
-	}
+    public Vector2(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 
-	public float getMagnitude() {
-		return (float) Math.sqrt(x * x + y * y);
-	}
+    public float getMagnitude() {
+        return (float) Math.sqrt(x * x + y * y);
+    }
 
-	public float getDot(Vector2 other) {
-		return x * other.x + y * other.y;
-	}
+    public float getDot(Vector2 other) {
+        return x * other.x + y * other.y;
+    }
 
-	public Vector2 setZero() {
-		x = 0.0F;
-		y = 0.0F;
+    public Vector2 setZero() {
+        x = 0.0F;
+        y = 0.0F;
 
-		return this;
-	}
+        return this;
+    }
 
-	public Vector2 add(Vector3 other) {
-		x += other.x;
-		y += other.y;
+    public Vector2 add(Vector3 other) {
+        x += other.x;
+        y += other.y;
 
-		return this;
-	}
+        return this;
+    }
 
-	public Vector2 subtract(Vector3 other) {
-		x -= other.x;
-		y -= other.y;
+    public Vector2 subtract(Vector3 other) {
+        x -= other.x;
+        y -= other.y;
 
-		return this;
-	}
+        return this;
+    }
 
-	public Vector2 negate() {
-		x = -x;
-		y = -y;
+    public Vector2 negate() {
+        x = -x;
+        y = -y;
 
-		return this;
-	}
+        return this;
+    }
 
-	public Vector2 normalize() {
-		float magnitude = getMagnitude();
+    public Vector2 normalize() {
+        float magnitude = getMagnitude();
 
-		x /= magnitude;
-		y /= magnitude;
+        x /= magnitude;
+        y /= magnitude;
 
-		return this;
-	}
+        return this;
+    }
+
+    public float[] toArray() {
+        return new float[] { x, y };
+    }
 
 }
