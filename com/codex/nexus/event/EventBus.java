1045d02bfea2fa2dad516272701be1dc2932f533
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-06 13:54:21 -0400

    Update EventBus.java

--- com/codex/nexus/event/EventBus.java
+++ com/codex/nexus/event/EventBus.java
@@ -11,61 +11,61 @@ import java.util.concurrent.CopyOnWriteArraySet;
 
 public class EventBus {
 
-	private Map<Class<?>, Set<Invocation>> subscribers;
+    private Map<Class<?>, Set<Invocation>> subscribers;
 
-	public EventBus() {
-		subscribers = new ConcurrentHashMap<>();
-	}
+    public EventBus() {
+        subscribers = new ConcurrentHashMap<>();
+    }
 
-	public void register(Object object) {
-		Class<?> clazz = object.getClass();
+    public void register(Object object) {
+        Class<?> clazz = object.getClass();
 
-		while (clazz != null) {
-			List<Method> methods = findWithParameterCount(1, findWithAnnotation(Subscribe.class, clazz));
+        while (clazz != null) {
+            List<Method> methods = findWithParameterCount(1, findWithAnnotation(Subscribe.class, clazz));
 
-			methods.forEach(method -> {
-				Class<?> parameterType = method.getParameterTypes()[0];
+            methods.forEach(method -> {
+                Class<?> parameterType = method.getParameterTypes()[0];
 
-				subscribers.computeIfAbsent(parameterType, key -> new CopyOnWriteArraySet<>())
-						.add(new Invocation(method, object));
-			});
+                subscribers.computeIfAbsent(parameterType, key -> new CopyOnWriteArraySet<>())
+                    .add(new Invocation(method, object));
+            });
 
-			clazz = clazz.getSuperclass();
-		}
-	}
+            clazz = clazz.getSuperclass();
+        }
+    }
 
-	public void unregister(Object object) {
-		Class<?> clazz = object.getClass();
+    public void unregister(Object object) {
+        Class<?> clazz = object.getClass();
 
-		while (clazz != null) {
-			List<Method> methods = findWithParameterCount(1, findWithAnnotation(Subscribe.class, clazz));
+        while (clazz != null) {
+            List<Method> methods = findWithParameterCount(1, findWithAnnotation(Subscribe.class, clazz));
 
-			methods.forEach(method -> {
-				Class<?> parameterType = method.getParameterTypes()[0];
+            methods.forEach(method -> {
+                Class<?> parameterType = method.getParameterTypes()[0];
 
-				if (subscribers.containsKey(parameterType)) {
-					Set<Invocation> set = subscribers.get(parameterType);
+                if (subscribers.containsKey(parameterType)) {
+                    Set<Invocation> set = subscribers.get(parameterType);
 
-					set.remove(new Invocation(method, object));
+                    set.remove(new Invocation(method, object));
 
-					if (set.isEmpty()) {
-						subscribers.remove(parameterType);
-					}
-				}
-			});
+                    if (set.isEmpty()) {
+                        subscribers.remove(parameterType);
+                    }
+                }
+            });
 
-			clazz = clazz.getSuperclass();
-		}
-	}
+            clazz = clazz.getSuperclass();
+        }
+    }
 
-	public void publish(Object object) {
-		Class<?> clazz = object.getClass();
+    public void publish(Object object) {
+        Class<?> clazz = object.getClass();
 
-		if (subscribers.containsKey(clazz)) {
-			Set<Invocation> invocations = subscribers.get(clazz);
+        if (subscribers.containsKey(clazz)) {
+            Set<Invocation> invocations = subscribers.get(clazz);
 
-			invocations.forEach(invocation -> invocation.invoke(object));
-		}
-	}
+            invocations.forEach(invocation -> invocation.invoke(object));
+        }
+    }
 
 }
