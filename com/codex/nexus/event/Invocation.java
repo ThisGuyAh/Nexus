e8e7e63431566f6df51fe20993f6d31ab10e03d6
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-06 13:54:46 -0400

    Update Invocation.java

--- com/codex/nexus/event/Invocation.java
+++ com/codex/nexus/event/Invocation.java
@@ -6,49 +6,49 @@ import java.util.Objects;
 
 public class Invocation {
 
-	private Method method;
-	private Object object;
+    private Method method;
+    private Object object;
 
-	public Invocation(Method method, Object object) {
-		this.method = method;
-		this.object = object;
-	}
+    public Invocation(Method method, Object object) {
+        this.method = method;
+        this.object = object;
+    }
 
-	public Method getMethod() {
-		return method;
-	}
+    public Method getMethod() {
+        return method;
+    }
 
-	public Object getObject() {
-		return object;
-	}
+    public Object getObject() {
+        return object;
+    }
 
-	public void invoke(Object... parameters) {
-		try {
-			method.setAccessible(true);
-			method.invoke(object, parameters);
-		} catch (IllegalAccessException | InvocationTargetException exception) {
-			exception.printStackTrace();
-		}
-	}
+    public void invoke(Object... parameters) {
+        try {
+            method.setAccessible(true);
+            method.invoke(object, parameters);
+        } catch (IllegalAccessException | InvocationTargetException exception) {
+            exception.printStackTrace();
+        }
+    }
 
-	@Override
-	public boolean equals(Object other) {
-		if (this == other) {
-			return true;
-		}
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
 
-		if (other == null || getClass() != other.getClass()) {
-			return false;
-		}
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
 
-		Invocation invocation = (Invocation) other;
+        Invocation invocation = (Invocation) other;
 
-		return Objects.equals(method, invocation.method) && Objects.equals(object, invocation.object);
-	}
+        return Objects.equals(method, invocation.method) && Objects.equals(object, invocation.object);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(method, object);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(method, object);
+    }
 
 }
