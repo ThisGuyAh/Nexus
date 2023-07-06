7a2abfbb89acd3747d8a35cb9ff0c6e4fbc0135a
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-06 13:48:28 -0400

    Update WindowResizeEvent.java

--- com/codex/nexus/event/WindowResizeEvent.java
+++ com/codex/nexus/event/WindowResizeEvent.java
@@ -4,26 +4,26 @@ import com.codex.nexus.core.Window;
 
 public class WindowResizeEvent {
 
-	private Window window;
-	private int oldWidth;
-	private int oldHeight;
-	
-	public WindowResizeEvent(Window window, int oldWidth, int oldHeight) {
-		this.window = window;
-		this.oldWidth = oldWidth;
-		this.oldHeight = oldHeight;
-	}
-	
-	public Window getWindow() {
-		return window;
-	}
+    private Window window;
+    private int oldWidth;
+    private int oldHeight;
 
-	public int getOldWidth() {
-		return oldWidth;
-	}
+    public WindowResizeEvent(Window window, int oldWidth, int oldHeight) {
+        this.window = window;
+        this.oldWidth = oldWidth;
+        this.oldHeight = oldHeight;
+    }
 
-	public int getOldHeight() {
-		return oldHeight;
-	}
+    public Window getWindow() {
+        return window;
+    }
+
+    public int getOldWidth() {
+        return oldWidth;
+    }
+
+    public int getOldHeight() {
+        return oldHeight;
+    }
 
 }
