b58c6efa06f6d2f2d091e8b1e2bae26904b0f3fb
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-06 13:59:41 -0400

    Update VertexBuffer.java

--- com/codex/nexus/memory/VertexBuffer.java
+++ com/codex/nexus/memory/VertexBuffer.java
@@ -5,104 +5,94 @@ import static org.lwjgl.opengl.GL15.*;
 
 /**
  * {@code VertexBuffer} stores vertex data and an {@code InputLayout}.
- * 
- * @see InputLayout
- * @see VertexArray
- * 
+ *
  * @author Christopher Ruley
  */
 public class VertexBuffer {
 
-	/**
-	 * The stored data.
-	 */
-	private float[] data;
+    /**
+     * The stored data.
+     */
+    private float[] data;
 
-	/**
-	 * The {@code InputLayout}.
-	 */
-	private InputLayout inputLayout;
+    /**
+     * The {@code InputLayout}.
+     */
+    private InputLayout inputLayout;
 
-	/**
-	 * The unique identification.
-	 */
-	private int handle;
+    private int handle;
 
-	/**
-	 * Constructs a {@code VertexBuffer}.
-	 * 
-	 * @param data the data to store.
-	 * @param inputLayout the {@code InputLayout}.
-	 */
-	public VertexBuffer(float[] data, InputLayout inputLayout) {
-		this.data = data;
-		this.inputLayout = inputLayout;
-		handle = glGenBuffers();
+    /**
+     * Constructs a {@code VertexBuffer}.
+     *
+     * @param data        the data to store.
+     * @param inputLayout the {@code InputLayout}.
+     */
+    public VertexBuffer(float[] data, InputLayout inputLayout) {
+        this.data = data;
+        this.inputLayout = inputLayout;
+        handle = glGenBuffers();
 
-		bind();
-		glBufferData(GL_ARRAY_BUFFER,
-				createFloatBuffer(data.length).put(data).flip(),
-				GL_STATIC_DRAW);
-	}
+        bind();
+        glBufferData(GL_ARRAY_BUFFER, createFloatBuffer(data.length).put(data).flip(), GL_STATIC_DRAW);
+    }
 
-	/**
-	 * Gets the stored data.
-	 * 
-	 * @return the stored data.
-	 */
-	public float[] getData() {
-		return data;
-	}
+    /**
+     * Gets the stored data.
+     *
+     * @return the stored data.
+     */
+    public float[] getData() {
+        return data;
+    }
 
-	/**
-	 * Gets the {@code InputLayout}.
-	 * 
-	 * @return the {@code InputLayout}.
-	 */
-	public InputLayout getInputLayout() {
-		return inputLayout;
-	}
-	
-	/**
-	 * Gets the unique identification.
-	 * 
-	 * @return the unique identification.
-	 */
-	public int getHandle() {
-		return handle;
-	}
+    /**
+     * Gets the {@code InputLayout}.
+     *
+     * @return the {@code InputLayout}.
+     */
+    public InputLayout getInputLayout() {
+        return inputLayout;
+    }
 
-	/**
-	 * Sets the stored data.
-	 * 
-	 * @param data the data to store.
-	 */
-	public void setData(float[] data) {
-		this.data = data;
+    /**
+     * Gets the unique identification.
+     *
+     * @return the unique identification.
+     */
+    public int getHandle() {
+        return handle;
+    }
 
-		bind();
-		glBufferSubData(GL_ARRAY_BUFFER, 0, data);
-	}
+    /**
+     * Sets the stored data.
+     *
+     * @param data the data to store.
+     */
+    public void setData(float[] data) {
+        bind();
+        glBufferSubData(GL_ARRAY_BUFFER, 0, this.data = data);
+    }
 
-	/**
-	 * Binds this {@code VertexBuffer}.
-	 */
-	public void bind() {
-		glBindBuffer(GL_ARRAY_BUFFER, handle);
-	}
+    /**
+     * Binds this {@code VertexBuffer}.
+     */
+    public void bind() {
+        glBindBuffer(GL_ARRAY_BUFFER, handle);
+    }
 
-	/**
-	 * Unbinds this {@code VertexBuffer}.
-	 */
-	public void unbind() {
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-	}
+    /**
+     * Unbinds this {@code VertexBuffer}.
+     */
+    public void unbind() {
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
 
-	/**
-	 * Deletes this {@code VertexBuffer}.
-	 */
-	public void delete() {
-		glDeleteBuffers(handle);
-	}
+    /**
+     * Deletes this {@code VertexBuffer}.
+     */
+    public void delete() {
+        glDeleteBuffers(handle);
+    }
 
 }
