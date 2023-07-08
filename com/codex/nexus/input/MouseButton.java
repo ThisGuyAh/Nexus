a2439d9dae3bd37d9a10e96b8642c1d17220663f
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-08 18:10:36 -0400

    Create MouseButton.java

---
+++ com/codex/nexus/input/MouseButton.java
@@ -0,0 +1,45 @@
+package com.codex.nexus.input;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+public enum MouseButton {
+
+    LEFT(GLFW_MOUSE_BUTTON_LEFT),
+    RIGHT(GLFW_MOUSE_BUTTON_RIGHT),
+    MIDDLE(GLFW_MOUSE_BUTTON_MIDDLE),
+    ONE(GLFW_MOUSE_BUTTON_1),
+    TWO(GLFW_MOUSE_BUTTON_2),
+    THREE(GLFW_MOUSE_BUTTON_3),
+    FOUR(GLFW_MOUSE_BUTTON_4),
+    FIVE(GLFW_MOUSE_BUTTON_5),
+    SIX(GLFW_MOUSE_BUTTON_6),
+    SEVEN(GLFW_MOUSE_BUTTON_7),
+    EIGHT(GLFW_MOUSE_BUTTON_8),
+    LAST(GLFW_MOUSE_BUTTON_LAST);
+
+    private final int code;
+
+    private static final Map<Integer, MouseButton> glfwTypes = new HashMap<>();
+
+    static {
+        for (var mouseButton : values()) {
+            glfwTypes.put(mouseButton.code, mouseButton);
+        }
+    }
+
+    MouseButton(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static MouseButton getFromGLFWType(int code) {
+        return glfwTypes.get(code);
+    }
+
+}
