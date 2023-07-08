ba00eebc641f762922f16e137a37f683cf51c27a
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-08 19:13:38 -0400

    Update KeyPressEvent.java

--- com/codex/nexus/event/KeyPressEvent.java
+++ com/codex/nexus/event/KeyPressEvent.java
@@ -1,23 +1,35 @@
 package com.codex.nexus.event;
 
+import com.codex.nexus.input.Key;
 import com.codex.nexus.core.Window;
 
 public class KeyPressEvent {
 
     private Window window;
-    private int code;
+    private Key key;
+    private boolean repeat;
 
-    public KeyPressEvent(Window window, int code) {
+    public KeyPressEvent(Window window, Key key, boolean repeat) {
         this.window = window;
-        this.code = code;
+        this.key = key;
+        this.repeat = repeat;
     }
 
     public Window getWindow() {
         return window;
     }
 
-    public int getCode() {
-        return code;
+    public Key getKey() {
+        return key;
+    }
+
+    public boolean isRepeat() {
+        return repeat;
+    }
+
+    @Override
+    public String toString() {
+        return "KeyPressEvent: " + key;
     }
 
 }
