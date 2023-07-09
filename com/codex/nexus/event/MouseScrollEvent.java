ac55411864d6c0ff1ce4b6a589afe6e16babc62b
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-09 02:11:46 -0400

    Update MouseScrollEvent.java

--- com/codex/nexus/event/MouseScrollEvent.java
+++ com/codex/nexus/event/MouseScrollEvent.java
@@ -1,24 +1,45 @@
 package com.codex.nexus.event;
 
-import com.codex.nexus.core.Window;
 import com.codex.nexus.math.Vector2;
 
+/**
+ * A {@code MouseScrollEvent} notifies subscribers when the mouse scrolls.
+ *
+ * @author Christopher Ruley
+ */
 public class MouseScrollEvent {
 
-    private Window window;
+    /**
+     * The mouse scroll offset.
+     */
     private Vector2 offset;
 
-    public MouseScrollEvent(Window window, Vector2 offset) {
-        this.window = window;
+    /**
+     * Constructs a {@code MouseScrollEvent}.
+     *
+     * @param offset the mouse scroll offset.
+     */
+    public MouseScrollEvent(Vector2 offset) {
         this.offset = offset;
     }
 
-    public Window getWindow() {
-        return window;
-    }
-
+    /**
+     * Gets the mouse scroll offset.
+     *
+     * @return the mouse scroll offset.
+     */
     public Vector2 getOffset() {
         return offset;
+    }
+
+    /**
+     * Gets a {@code String} representation of this {@code MouseScrollEvent}.
+     *
+     * @return a {@code String} representation of this {@code MouseScrollEvent}.
+     */
+    @Override
+    public String toString() {
+        return "MouseScrollEvent: " + offset.toString();
     }
 
 }
