f4a6c5e521176e7f201a8b5b89fdce5c297db6e3
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-09 02:11:16 -0400

    Update MouseMoveEvent.java

--- com/codex/nexus/event/MouseMoveEvent.java
+++ com/codex/nexus/event/MouseMoveEvent.java
@@ -1,24 +1,45 @@
 package com.codex.nexus.event;
 
-import com.codex.nexus.core.Window;
 import com.codex.nexus.math.Vector2;
 
+/**
+ * A {@code MouseMoveEvent} notifies subscribers when the mouse moves.
+ *
+ * @author Christopher Ruley
+ */
 public class MouseMoveEvent {
 
-    private Window window;
+    /**
+     * The mouse position.
+     */
     private Vector2 position;
 
-    public MouseMoveEvent(Window window, Vector2 position) {
-        this.window = window;
+    /**
+     * Constructs a {@code MouseMoveEvent}.
+     *
+     * @param position the mouse position.
+     */
+    public MouseMoveEvent(Vector2 position) {
         this.position = position;
     }
 
-    public Window getWindow() {
-        return window;
-    }
-
+    /**
+     * Gets the mouse position.
+     *
+     * @return the mouse position.
+     */
     public Vector2 getPosition() {
         return position;
+    }
+
+    /**
+     * Gets a {@code String} representation of this {@code MouseMoveEvent}.
+     *
+     * @return a {@code String} representation of this {@code MouseMoveEvent}.
+     */
+    @Override
+    public String toString() {
+        return "MouseMoveEvent: " + position.toString();
     }
 
 }
