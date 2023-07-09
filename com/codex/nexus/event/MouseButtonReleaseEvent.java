8b35b34a514a3232355da54754990a1b6df2cfb2
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-09 01:19:40 -0400

    Update MouseButtonReleaseEvent.java

--- com/codex/nexus/event/MouseButtonReleaseEvent.java
+++ com/codex/nexus/event/MouseButtonReleaseEvent.java
@@ -1,26 +1,43 @@
 package com.codex.nexus.event;
 
-import com.codex.nexus.core.Window;
 import com.codex.nexus.input.MouseButton;
 
+/**
+ * A {@code MouseButtonReleaseEvent} notifies subscribers when a {@code MouseButton} is released.
+ *
+ * @author Christopher Ruley
+ */
 public class MouseButtonReleaseEvent {
 
-    private Window window;
+    /**
+     * The {@code MouseButton} released.
+     */
     private MouseButton mouseButton;
 
-    public MouseButtonReleaseEvent(Window window, MouseButton mouseButton) {
-        this.window = window;
+    /**
+     * Constructs a {@code MouseButtonReleaseEvent}.
+     *
+     * @param mouseButton the {@code MouseButton} released.
+     */
+    public MouseButtonReleaseEvent(MouseButton mouseButton) {
         this.mouseButton = mouseButton;
     }
 
-    public Window getWindow() {
-        return window;
-    }
-
+    /**
+     * Gets the {@code MouseButton} released.
+     *
+     * @return the {@code MouseButton} released.
+     */
     public MouseButton getMouseButton() {
         return mouseButton;
     }
 
+    /**
+     * Gets a {@code String} representation of this {@code MouseButtonReleaseEvent}, including the {@code MouseButton}
+     * released.
+     *
+     * @return a {@code String} representation of this {@code MouseButtonReleaseEvent}.
+     */
     @Override
     public String toString() {
         return "MouseButtonReleaseEvent: " + mouseButton;
