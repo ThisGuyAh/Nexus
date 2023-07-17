ff4878b52cbba7b3ab1673938d282f7563397f26
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-17 13:04:04 -0400

    Update MouseScrollEvent.java

--- com/codex/nexus/event/MouseScrollEvent.java
+++ com/codex/nexus/event/MouseScrollEvent.java
@@ -1,5 +1,6 @@
 package com.codex.nexus.event;
 
+import com.codex.nexus.core.Window;
 import com.codex.nexus.math.Vector2;
 
 /**
@@ -10,6 +11,11 @@ import com.codex.nexus.math.Vector2;
 public class MouseScrollEvent {
 
     /**
+     * The {@code Window}.
+     */
+    private Window window;
+
+    /**
      * The mouse scroll offset.
      */
     private Vector2 offset;
@@ -17,10 +23,21 @@ public class MouseScrollEvent {
     /**
      * Constructs a {@code MouseScrollEvent}.
      *
+     * @param window the {@code Window}.
      * @param offset the mouse scroll offset.
      */
-    public MouseScrollEvent(Vector2 offset) {
+    public MouseScrollEvent(Window window, Vector2 offset) {
+        this.window = window;
         this.offset = offset;
+    }
+
+    /**
+     * Gets the {@code Window}.
+     *
+     * @return window the {@code Window}.
+     */
+    public Window getWindow() {
+        return window;
     }
 
     /**
@@ -39,7 +56,7 @@ public class MouseScrollEvent {
      */
     @Override
     public String toString() {
-        return "MouseScrollEvent: " + offset.toString();
+        return "MouseScrollEvent: Window = " + window.getTitle() + ", " + offset.toString();
     }
 
 }
