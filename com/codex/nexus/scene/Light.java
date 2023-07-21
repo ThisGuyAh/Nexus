54c2858cd56a01c145ce710b7bf0865067971a3c
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-21 16:19:17 -0400

    Create Light.java

---
+++ com/codex/nexus/scene/Light.java
@@ -0,0 +1,32 @@
+package com.codex.nexus.scene;
+
+import com.codex.nexus.math.Vector3;
+import com.codex.nexus.math.Vector4;
+
+public class Light {
+
+    private Vector3 position;
+    private Vector4 color;
+
+    public Light(Vector3 position, Vector4 color) {
+        this.position = position;
+        this.color = color;
+    }
+
+    public Vector3 getPosition() {
+        return position;
+    }
+
+    public Vector4 getColor() {
+        return color;
+    }
+
+    public void setPosition(Vector3 position) {
+        this.position = position;
+    }
+
+    public void setColor(Vector4 color) {
+        this.color = color;
+    }
+
+}
