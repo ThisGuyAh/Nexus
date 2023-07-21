d085d2008ae9f73b1037077acc334ea7073a0e64
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-21 16:18:58 -0400

    Create Entity.java

---
+++ com/codex/nexus/scene/Entity.java
@@ -0,0 +1,36 @@
+package com.codex.nexus.scene;
+
+import com.codex.nexus.math.Vector3;
+import com.codex.nexus.model.Model;
+
+public class Entity {
+
+    private Model model;
+    private Vector3 position;
+    private Vector3 rotation;
+    private Vector3 scale;
+
+    public Entity(Model model, Vector3 position, Vector3 rotation, Vector3 scale) {
+        this.model = model;
+        this.position = position;
+        this.rotation = rotation;
+        this.scale = scale;
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    public Vector3 getPosition() {
+        return position;
+    }
+
+    public Vector3 getRotation() {
+        return rotation;
+    }
+
+    public Vector3 getScale() {
+        return scale;
+    }
+
+}
