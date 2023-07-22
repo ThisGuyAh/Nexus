a6c25c4befad4259bb5f9ca7c8d0070184ae1bfc
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-22 13:46:13 -0400

    Update Matrix2.java

--- com/codex/nexus/math/Matrix2.java
+++ com/codex/nexus/math/Matrix2.java
@@ -1,6 +1,6 @@
 package com.codex.nexus.math;
 
-public class Matrix2 {
+public class Matrix2 extends Matrix {
 
     public float element00;
     public float element01;
@@ -107,6 +107,7 @@ public class Matrix2 {
         return this;
     }
 
+    @Override
     public float[] toArray() {
         return new float[] {
             element00,
