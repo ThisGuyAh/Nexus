ef6c18b77cd7ac2215f68463101a505907b0d79c
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-22 13:47:37 -0400

    Update Vector2.java

--- com/codex/nexus/math/Vector2.java
+++ com/codex/nexus/math/Vector2.java
@@ -1,97 +1,77 @@
 package com.codex.nexus.math;
 
-public class Vector3 extends Vector {
+public class Vector2 extends Vector {
 
     public float x;
     public float y;
-    public float z;
 
-    public Vector3() {
+    public Vector2(){
         setZero();
     }
 
-    public Vector3(Vector3 other) {
+    public Vector2(Vector2 other) {
         x = other.x;
         y = other.y;
-        z = other.z;
     }
 
-    public Vector3(float x, float y, float z) {
+    public Vector2(float x, float y) {
         this.x = x;
         this.y = y;
-        this.z = z;
     }
 
     public float getMagnitude() {
-        return (float) Math.sqrt(x * x + y * y + z * z);
+        return (float) Math.sqrt(x * x + y * y);
     }
 
-    public float getDot(Vector3 other) {
-        return x * other.x + y * other.y + z * other.z;
+    public float getDot(Vector2 other) {
+        return x * other.x + y * other.y;
     }
 
-    public Vector3 setZero() {
+    public Vector2 setZero() {
         x = 0.0F;
         y = 0.0F;
-        z = 0.0F;
 
         return this;
     }
 
-    public Vector3 add(Vector3 other) {
+    public Vector2 add(Vector2 other) {
         x += other.x;
         y += other.y;
-        z += other.z;
 
         return this;
     }
 
-    public Vector3 subtract(Vector3 other) {
+    public Vector2 subtract(Vector2 other) {
         x -= other.x;
         y -= other.y;
-        z -= other.z;
 
         return this;
     }
 
-    public Vector3 cross(Vector3 other) {
-        float a = y * other.z - z * other.y;
-        float b = other.x * z - other.z * x;
-        float c = x * other.y - y * other.x;
-
-        x = a;
-        y = b;
-        z = c;
-
-        return this;
-    }
-
-    public Vector3 negate() {
+    public Vector2 negate() {
         x = -x;
         y = -y;
-        z = -z;
 
         return this;
     }
 
-    public Vector3 normalize() {
+    public Vector2 normalize() {
         float magnitude = getMagnitude();
 
         x /= magnitude;
         y /= magnitude;
-        z /= magnitude;
 
         return this;
     }
 
     @Override
     public float[] toArray() {
-        return new float[] { x, y, z };
+        return new float[] { x, y };
     }
 
     @Override
     public String toString() {
-        return "X = " + x + ", Y = " + y + ", Z = " + z;
+        return "X = " + x + ", Y = " + y;
     }
 
 }
