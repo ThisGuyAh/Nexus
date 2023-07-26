0d1cbe6bdee8ec9b3f3838e96225dde213b0fbc4
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-07-26 08:39:17 -0400

    Create Model.java

---
+++ com/codex/nexus/model/Model.java
@@ -0,0 +1,15 @@
+package com.codex.nexus.model;
+
+public class Model {
+
+    private Mesh[] meshes;
+
+    public Model(Mesh[] meshes) {
+        this.meshes = meshes;
+    }
+
+    public Mesh[] getMeshes() {
+        return meshes;
+    }
+
+}
