701fd4947d2e56db81c5e3c577a8c1903e0cb399
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-09-11 17:53:10 -0400

    Update WindowFocusEvent.java

--- com/codex/nexus/event/WindowFocusEvent.java
+++ com/codex/nexus/event/WindowFocusEvent.java
@@ -2,11 +2,29 @@ package com.codex.nexus.event;
 
 import com.codex.nexus.core.Window;
 
+/**
+ * A {@code WindowFocusEvent} notifies subscribers when a {@code Window} gains or loses focus.
+ *
+ * @author Christopher Ruley
+ */
 public class WindowFocusEvent {
 
+    /**
+     * The {@code Window}.
+     */
     private Window window;
+
+    /**
+     * Whether focus was gained or lost.
+     */
     private boolean focused;
 
+    /**
+     * Instantiates a {@code WindowFocusEvent}.
+     *
+     * @param window  the {@code Window}.
+     * @param focused whether focus was gained or lost.
+     */
     public WindowFocusEvent(Window window, boolean focused) {
         this.window = window;
         this.focused = focused;
@@ -18,6 +36,11 @@ public class WindowFocusEvent {
 
     public boolean isFocused() {
         return focused;
+    }
+
+    @Override
+    public String toString() {
+        return "WindowFocusEvent: Window = " + window.getTitle() + ", Focused = " + focused;
     }
 
 }
