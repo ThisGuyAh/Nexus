d2ff5f5b65669b12b9f5778665fbf8ef8620273b
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-09-11 17:35:32 -0400

    Update MouseScrollEvent.java

--- com/codex/nexus/event/MouseScrollEvent.java
+++ com/codex/nexus/event/MouseScrollEvent.java
@@ -4,11 +4,11 @@ import com.codex.nexus.core.Window;
 import com.codex.nexus.math.Vector2;
 
 /**
- * A {@code MouseScrollEvent} notifies subscribers when the mouse scrolls.
+ * A {@code MouseWheelScrollEvent} notifies subscribers when the mouse wheel scrolls.
  *
  * @author Christopher Ruley
  */
-public class MouseScrollEvent {
+public class MouseWheelScrollEvent {
 
     /**
      * The {@code Window}.
@@ -16,7 +16,7 @@ public class MouseScrollEvent {
     private Window window;
 
     /**
-     * The mouse scroll offset.
+     * The mouse wheel scroll offset.
      */
     private Vector2 offset;
 
@@ -24,39 +24,24 @@ public class MouseScrollEvent {
      * Constructs a {@code MouseScrollEvent}.
      *
      * @param window the {@code Window}.
-     * @param offset the mouse scroll offset.
+     * @param offset the mouse wheel scroll offset.
      */
-    public MouseScrollEvent(Window window, Vector2 offset) {
+    public MouseWheelScrollEvent(Window window, Vector2 offset) {
         this.window = window;
         this.offset = offset;
     }
 
-    /**
-     * Gets the {@code Window}.
-     *
-     * @return window the {@code Window}.
-     */
     public Window getWindow() {
         return window;
     }
 
-    /**
-     * Gets the mouse scroll offset.
-     *
-     * @return the mouse scroll offset.
-     */
     public Vector2 getOffset() {
         return offset;
     }
 
-    /**
-     * Gets a {@code String} representation of this {@code MouseScrollEvent}.
-     *
-     * @return a {@code String} representation of this {@code MouseScrollEvent}.
-     */
     @Override
     public String toString() {
-        return "MouseScrollEvent: Window = " + window.getTitle() + ", " + offset.toString();
+        return "MouseScrollEvent: Window = " + window.getTitle() + ", " + offset;
     }
 
 }
