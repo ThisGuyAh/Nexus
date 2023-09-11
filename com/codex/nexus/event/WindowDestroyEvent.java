cff12944de5f614806914dd9677d335ef38d8b44
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-09-11 18:12:51 -0400

    Update WindowDestroyEvent.java

--- com/codex/nexus/event/WindowDestroyEvent.java
+++ com/codex/nexus/event/WindowDestroyEvent.java
@@ -2,16 +2,34 @@ package com.codex.nexus.event;
 
 import com.codex.nexus.core.Window;
 
+/**
+ * A {@code WindowDestroyEvent} notifies subscribers when a {@code Window} is created.
+ *
+ * @author Christopher Ruley
+ */
 public class WindowDestroyEvent {
 
+    /**
+     * The {@code Window}.
+     */
     private Window window;
 
+    /**
+     * Instantiates a {@code WindowDestroyEvent}.
+     *
+     * @param window the {@code Window}.
+     */
     public WindowDestroyEvent(Window window) {
         this.window = window;
     }
 
     public Window getWindow() {
         return window;
+    }
+
+    @Override
+    public String toString() {
+        return "WindowDestroyEvent: Window = " + window.getTitle();
     }
 
 }
