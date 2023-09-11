c31ee3f2356cc36185e150908ef869cc086431bc
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-09-11 18:11:53 -0400

    Update WindowCreateEvent.java

--- com/codex/nexus/event/WindowCreateEvent.java
+++ com/codex/nexus/event/WindowCreateEvent.java
@@ -2,16 +2,34 @@ package com.codex.nexus.event;
 
 import com.codex.nexus.core.Window;
 
+/**
+ * A {@code WindowCreateEvent} notifies subscribers when a {@code Window} is created.
+ * 
+ * @author Christopher Ruley
+ */
 public class WindowCreateEvent {
 
+    /**
+     * The {@code Window}.
+     */
     private Window window;
 
+    /**
+     * Instantiates a {@code WindowCreateEvent}.
+     * 
+     * @param window the {@code Window}.
+     */
     public WindowCreateEvent(Window window) {
         this.window = window;
     }
 
     public Window getWindow() {
         return window;
+    }
+
+    @Override
+    public String toString() {
+        return "WindowCreateEvent: Window = " + window.getTitle();
     }
 
 }
