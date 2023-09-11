fda7c31fd4eded83032b485d6850eb37dc8217be
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2023-09-11 17:52:58 -0400

    Update WindowResizeEvent.java

--- com/codex/nexus/event/WindowResizeEvent.java
+++ com/codex/nexus/event/WindowResizeEvent.java
@@ -2,12 +2,35 @@ package com.codex.nexus.event;
 
 import com.codex.nexus.core.Window;
 
+/**
+ * A {@code WindowFocusEvent} notifies subscribers when a {@code Window} resizes.
+ *
+ * @author Christopher Ruley
+ */
 public class WindowResizeEvent {
 
+    /**
+     * The {@code Window}.
+     */
     private Window window;
+
+    /**
+     * The old width.
+     */
     private int oldWidth;
+
+    /**
+     * The old height.
+     */
     private int oldHeight;
 
+    /**
+     * Instantiates a {@code WindowResizeEvent}.
+     * 
+     * @param window the {@code Window}.
+     * @param oldWidth the old width.
+     * @param oldHeight the old height.
+     */
     public WindowResizeEvent(Window window, int oldWidth, int oldHeight) {
         this.window = window;
         this.oldWidth = oldWidth;
@@ -24,6 +47,12 @@ public class WindowResizeEvent {
 
     public int getOldHeight() {
         return oldHeight;
+    }
+
+    @Override
+    public String toString() {
+        return "WindowResizeEvent: Window = " + window.getTitle() + ", Width = " + window.getWidth() + ", Height = "
+            + window.getHeight() + ", OldWidth = " + oldWidth + ", OldHeight = " + oldHeight;
     }
 
 }
