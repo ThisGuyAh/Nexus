5aff3acc141df4f08a1dce451715bb3e4d0ccfd0
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2024-01-30 13:14:16 -0500

    Update Matrix.java

--- com/codex/nexus/math/Matrix.java
+++ com/codex/nexus/math/Matrix.java
@@ -1,7 +1,43 @@
 package com.codex.nexus.math;
 
+/**
+ * A {@code Matrix} is a base for a matrix representation.
+ */
 public abstract class Matrix {
 
+    /**
+     * Sets this {@code Matrix} to zero.
+     *
+     * @return this {@code Matrix}.
+     */
+    public abstract Matrix setZero();
+
+    /**
+     * Sets this {@code Matrix} to the identity.
+     *
+     * @return this {@code Matrix}.
+     */
+    public abstract Matrix setIdentity();
+
+    /**
+     * Negates this {@code Matrix}.
+     *
+     * @return this {@code Matrix}.
+     */
+    public abstract Matrix negate();
+
+    /**
+     * Inverts this {@code Matrix}.
+     *
+     * @return this {@code Matrix}.
+     */
+    public abstract Matrix invert();
+
+    /**
+     * Gets a float array from this {@code Matrix}.
+     *
+     * @return a float array from this {@code Matrix}.
+     */
     public abstract float[] toArray();
 
 }
