9d200a0c5936ce63b2161d2e63ee1c57efa64e86
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2024-01-30 20:47:03 -0500

    Update Vector.java

--- com/codex/nexus/math/Vector.java
+++ com/codex/nexus/math/Vector.java
@@ -1,11 +1,18 @@
 package com.codex.nexus.math;
 
 /**
- * A {@code Vector} is a base for a vector respresentation.
+ * A {@code Vector} is a base for a vector representation.
  *
  * @author Christopher Ruley
  */
 public abstract class Vector {
+
+    /**
+     * Gets the magnitude.
+     *
+     * @return the magnitude.
+     */
+    public abstract float getMagnitude();
 
     /**
      * Sets this {@code Vector} to zero.
@@ -13,6 +20,20 @@ public abstract class Vector {
      * @return this {@code Vector}.
      */
     public abstract Vector setZero();
+
+    /**
+     * Negates this {@code Vector}.
+     *
+     * @return this {@code Vector}.
+     */
+    public abstract Vector negate();
+
+    /**
+     * Normalizes this {@code Vector}.
+     *
+     * @return this {@code Vector}.
+     */
+    public abstract Vector normalize();
 
     /**
      * Gets a float array from this {@code Vector}.
