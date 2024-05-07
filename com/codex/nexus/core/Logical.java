9d855eb89cfc5676215d7ca1f8a5b1846a6b6a46
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2024-05-07 18:16:35 -0400

    Update Logical.java

--- com/codex/nexus/core/Logical.java
+++ com/codex/nexus/core/Logical.java
@@ -2,33 +2,33 @@ package com.codex.nexus.core;
 
 /**
  * {@code Logical} provides methods to be executed at various stages within {@code Application}.
- * 
+ *
  * @author Christopher Ruley
  */
 public interface Logical {
 
     /**
-     * Called during the initialize stage of {@code Application}.
+     * Called during the creation stage of {@code Application}.
      */
-    void onInitialize();
+    void onCreate();
 
     /**
-     * Called during the input stage of {@code Application}.
+     * Called during the input-polling stage of {@code Application}.
      */
     void onInput();
 
     /**
      * Called during the update stage of {@code Application}.
      */
-    void onUpdate();
+    void onUpdate(double delta);
 
     /**
      * Called during the render stage of {@code Application}.
      */
-    void onRender();
+    void onRender(double alpha);
 
     /**
-     * Called during the destroy stage of {@code Application}.
+     * Called during the destruction stage of {@code Application}.
      */
     void onDestroy();
 
