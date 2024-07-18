00afd16716167c0aaea49c8796c54c95efb3b2b5
Christopher Ruley <86430018+ThisGuyAh@users.noreply.github.com>
2024-07-17 22:14:53 -0400

    Update Mesh.java

--- com/codex/nexus/model/Mesh.java
+++ com/codex/nexus/model/Mesh.java
@@ -4,46 +4,52 @@ import com.codex.nexus.memory.VertexArray;
 
 public class Mesh {
 
-	private String name;
-	private VertexArray vertexArray;
-	private Material material;
+    private String name;
+    private VertexArray vertexArray;
+    private Material material;
+    private int vertexCount;
 
-	public Mesh(String name, VertexArray vertexArray, Material material) {
-		this.name = name;
-		this.vertexArray = vertexArray;
-		this.material = material;
-	}
+    public Mesh(String name, VertexArray vertexArray, Material material) {
+        this.name = name;
+        this.vertexArray = vertexArray;
+        this.material = material;
+        vertexCount = vertexArray.getIndexBuffer().getCount();
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public VertexArray getVertexArray() {
-		return vertexArray;
-	}
+    public VertexArray getVertexArray() {
+        return vertexArray;
+    }
 
-	public Material getMaterial() {
-		return material;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public void setMaterial(Material material) {
-		this.material = material;
-	}
+    public Material getMaterial() {
+        return material;
+    }
 
-	public void bind() {
-		vertexArray.bind();
-	}
+    public int getVertexCount() {
+        return vertexCount;
+    }
 
-	public void unbind() {
-		vertexArray.unbind();
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void delete() {
-		vertexArray.delete();
-	}
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public void bind() {
+        vertexArray.bind();
+    }
+
+    public void unbind() {
+        vertexArray.unbind();
+    }
+
+    public void delete() {
+        vertexArray.delete();
+    }
 
 }
